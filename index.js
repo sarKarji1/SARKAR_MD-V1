f2cad0b6017722ef6445b0af652a1a89aa9dd294
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2024-10-11 12:19:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const ownerNumber = ['923253617422']
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
 const sessdata = config.SESSION_ID
-const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
+const filer = File.fromURL(`https://mega.nz/file/${sessdata}#${fileHash}`)
 filer.download((err, data) => {
 if(err) throw err
 fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
