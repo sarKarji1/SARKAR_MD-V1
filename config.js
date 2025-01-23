145600f675346d3980b85c6887fbd18c5e59ae09
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2025-01-23 06:04:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "Enter your Session id"
+
+    
 };
